24f4b1287897ac8892b5be94b265a64dbdb34b51
Yarne Decuyper <yarne.decuyper@student.howest.be>
2018-11-29 13:08:10 +0100

    Refactor shitty tim code :)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,77 +1,90 @@
 let player = {};
 let pyramids = [];
-function fetchMap(){
-    fetch("http://involved-htf-js-2018-prod.azurewebsites.net/api/challenge/3",{
-        method: "GET",
-        headers:{
-            "Content-Type":  "application/json",
-            "Accept": "application/json",
-            "x-team" : "lawyer"
-        }
-    }).then(function(response){
-        return response;
-    }).then(data => {
-        return data.json();
-    }).then(jsondata => {
-        drawGrid(jsondata.tiles);
-    })
-}
 
 $(document).ready(() => {
-    fetchMap();
-    $(document).keydown(function(e) {
-        let move = {};
-    switch(e.which) {
-        case 37: player.x -= 3;
-        break;
+    window.setInterval(() => {
+        renderData();
+    }, 500);
 
-        case 38: player.y -= 3;// up
-        break;
+    $(document).keydown(function (e) {
+        switch (e.which) {
+            case 37:
+                player.x -= 3;
+                break;
 
-        case 39: player.x += 3;// right
-        break;
+            case 38:
+                player.y -= 3;// up
+                break;
 
-        case 40: player.y += 3;// down
-        break;
+            case 39:
+                player.x += 3;// right
+                break;
 
-        default: return; // exit this handler for other keys
-    }
-        postPlayerLocation(player);
-        fetchMap();
-        if(pyramids.length == 3){
-            postPyramids();   
+            case 40:
+                player.y += 3;// down
+                break;
+
+            default:
+                return; // exit this handler for other keys
         }
+        postPlayerLocation(player);
         e.preventDefault(); // prevent the default action (scroll / move caret)
     });
-    
-    
-})
+});
 
-function drawGrid(field){
+
+
+
+function fetchMap() {
+    return fetch("http://involved-htf-js-2018-prod.azurewebsites.net/api/challenge/3", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "x-team": "lawyer"
+        }
+    }).then(function (response) {
+        return response;
+    }).then((data) => {
+        return data.json();
+    }).then((jsondata) => {
+        return jsondata.tiles;
+    });
+}
+
+async function renderData() {
+    console.log("rendering");
+    let tilesmap = await fetchMap();
+    drawGrid(tilesmap);
+}
+
+function drawGrid(field) {
     let canvas = document.getElementById("map");
     let ctx = canvas.getContext("2d");
     ctx.fillStyle = "#FF0000";
     pyramids = [];
-    for(let tile of field){
-        switch(tile.type){
-            case 1: ctx.fillStyle = "brown";
+    for (let tile of field) {
+        switch (tile.type) {
+            case 1:
+                ctx.fillStyle = "brown";
                 break;
-            case 2: ctx.fillStyle = "blue";
-                    player.x = tile.x;
-                    player.y = tile.y;
+            case 2:
+                ctx.fillStyle = "blue";
+                player.x = tile.x;
+                player.y = tile.y;
                 break;
-            case 3: ctx.fillStyle = "orange";
-                    pyramids.push({"x":tile.x, "y": tile.y});
+            case 3:
+                ctx.fillStyle = "orange";
+                pyramids.push({"x": tile.x, "y": tile.y});
                 break;
         }
-        ctx.fillRect(tile.x*10,tile.y*10,10,10);
+        ctx.fillRect(tile.x * 10, tile.y * 10, 10, 10);
     }
     drawBoard();
-    console.log("klaar me tekenen");
 }
 
 
-function drawBoard(){
+function drawBoard() {
     let bw = 800;
     let bh = 450;
     let p = 0;
@@ -93,32 +106,32 @@ function drawBoard(){
 }
 
 
-function postPlayerLocation(moveTo){
-    fetch("http://involved-htf-js-2018-prod.azurewebsites.net/api/challenge/3/move",{
+function postPlayerLocation(moveTo) {
+    fetch("http://involved-htf-js-2018-prod.azurewebsites.net/api/challenge/3/move", {
         method: "POST",
-        headers:{
-            "Content-Type":  "application/json",
+        headers: {
+            "Content-Type": "application/json",
             "Accept": "application/json",
-            "x-team" : "lawyer"
+            "x-team": "lawyer"
         },
         body: JSON.stringify(moveTo)
-        
-    }).then(function(response){
+
+    }).then(function (response) {
         return response;
     })
 }
 
-function postPyramids(){
-    fetch("http://involved-htf-js-2018-prod.azurewebsites.net/api/challenge/3",{
+function postPyramids() {
+    fetch("http://involved-htf-js-2018-prod.azurewebsites.net/api/challenge/3", {
         method: "POST",
-        headers:{
-            "Content-Type":  "application/json",
+        headers: {
+            "Content-Type": "application/json",
             "Accept": "application/json",
-            "x-team" : "lawyer"
+            "x-team": "lawyer"
         },
         body: JSON.stringify(pyramids)
-        
-    }).then(function(response){
+
+    }).then(function (response) {
         return response;
     })
 }
